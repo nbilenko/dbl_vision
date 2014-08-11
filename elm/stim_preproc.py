f014cddd3bdebbe42599c10ee9221311e7f6ba3f
nbilenko <nbilenko@berkeley.edu>
2014-08-10 18:16:37 -0700

    stim preproc

--- elm/stim_preproc.py
+++ elm/stim_preproc.py
@@ -6,7 +6,7 @@ imshape = (128, 128, 3)
 
 for sess in range(1, sessions+1):
 	for stim_type in ("trn", "val"):
-		stim = movie_utils.load_table_file(tfname % (stim_type, sess))["data"]
+		stim = movie_utils.load_table_file(fname % (stim_type, sess))["data"]
 		images = []
 		for imnum in range(stim.shape[1]):
 			images.append(stim[:, imnum].reshape((128, 128, 3), order ="F"))
